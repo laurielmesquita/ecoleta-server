03ced6fbcf85ae60add8730d9ccae1e63846a855
Lauriel Mesquita <laurielmesquita@me.com>
2020-06-08 17:48:02 -0300

    :rocket: Upload file is ready

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import express from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 import ItemsController from './controllers/ItemsController'
 import PointsController from './controllers/PointsController'
 
 const routes = express()
+const upload = multer(multerConfig)
+
 const pointsController = new PointsController()
 const itemsController = new ItemsController()
 
 routes.get('/items', itemsController.index)
-routes.post('/points', pointsController.create)
+routes.post('/points', upload.single('image'), pointsController.create)
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
